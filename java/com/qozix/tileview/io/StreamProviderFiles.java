afb0eb349dbc5cd7ba8296ee1e98801bdb29e4fe
dnagis <vincent.achard@gmail.com>
2019-04-07 19:07:03 +0200

    nom des fichiers ign-* et premiers essais accès fichiers à part pas en assets

--- java/com/qozix/tileview/io/StreamProviderFiles.java
+++ java/com/qozix/tileview/io/StreamProviderFiles.java
@@ -4,14 +4,27 @@ import android.content.Context;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.FileNotFoundException;
 import java.util.Locale;
+
+import android.util.Log;
+
+//for file in `ls *.jpg`; do adb push $file /sdcard/tiles/ ; done
 
 public class StreamProviderFiles implements StreamProvider {
 
   @Override
   public InputStream getStream(int column, int row, Context context, Object data) throws Exception {
     String file = String.format(Locale.US, (String) data, column, row);
-    return new FileInputStream(file);
+    
+    //try {
+   return new FileInputStream(file);
+	//} catch (FileNotFoundException ex) {
+   //Log.d("vvnx", "exception");
+	//}
+    
+    
+    //return is;
   }
 
 }
--- java/demo/src/main/java/tileview/demo/TileViewDemoSimple.java
+++ java/demo/src/main/java/tileview/demo/TileViewDemoSimple.java
@@ -3,6 +3,8 @@ package tileview.demo;
 import android.app.Activity;
 import android.os.Bundle;
 //import android.support.annotation.Nullable;
+import com.qozix.tileview.io.StreamProvider;
+import com.qozix.tileview.io.StreamProviderFiles;
 
 import com.qozix.tileview.TileView;
 
@@ -14,11 +16,19 @@ public class TileViewDemoSimple extends Activity {
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_demos_tileview);
+    
+ /**
+ * dans assets ils mettent phi-1000000-[0-69]_[0-52].jpg de 256*256 
+ * 69*256 = 17920
+ * 52*256 = 13312
+ */
 
     TileView tileView = findViewById(R.id.tileview);
     new TileView.Builder(tileView)
-        .setSize(17934, 13452)
-        .defineZoomLevel("tiles/phi-1000000-%1$d_%2$d.jpg")
+        .setSize(17934, 13452) 
+//        .defineZoomLevel("tiles/phi-1000000-%1$d_%2$d.jpg")
+        .defineZoomLevel("tiles/ign-%1$d_%2$d.jpg")
+        .setStreamProvider(new StreamProviderFiles())
         .build();
 
   }
