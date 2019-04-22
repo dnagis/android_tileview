f5f8f1d1241d84ed5ecf3c1e04adc4d78553cbc1
dnagis <vincent.achard@gmail.com>
2019-04-22 10:57:13 +0200

    makeup rapide

--- java/demo/src/main/java/tileview/demo/TileViewDemoSimple.java
+++ java/demo/src/main/java/tileview/demo/TileViewDemoSimple.java
@@ -33,18 +33,12 @@ public class TileViewDemoSimple extends Activity {
   //upper left tile : COL0=33478 ROW0=23948 43.53262042681010 3.90014648437500
   //si j'ai n tiles en horizontal (x, col) et m tiles en vertical (y, row), il faut calculer les coordonnées upper left (ce que latlng donne) de la première tile out of range en bas à droite
   // c'est à dire la tile n° x' y' avec x'=x+n et y'=y+m 
-  /*public static final double NORTH = 43.95723647202563;
-  public static final double WEST = 3.68041992187500;
-  public static final double SOUTH = 43.85829677916184;
-  public static final double EAST = 3.81774902343750;*/
+  /*public static final double NORTH = 43.95723647202563 ... WEST = 3.68041992187500 ... SOUTH = 43.85829677916184 EAST = 3.81774902343750;*/
   double WEST;
   double EAST;
   double NORTH;
   double SOUTH;
-  //43.5196571350098,3.91340827941895 marine du prevost au bout de la promenade 
-  //43.9341011047363,3.70944619178772 12 ru portail laroque
   double[] coordinates = new double[]{43.9341011047363,3.70944619178772};
-  //size: setSize(17934, 13452) //pour 69 col par 52 row, setSize(1792, 1280) //pour 7 col par 5 row, ...
   int n_tiles_x, n_tiles_y, col_0, row_0, sizePixelW, sizePixelH;
   
   TileView tileView;
@@ -63,7 +57,7 @@ public class TileViewDemoSimple extends Activity {
     //Log.d("vvnx", "getfilesdir=" + mContext.getFilesDir()); //me donne /data/user/0/tileview.demo/files
     
 	 /**
-	 * dans assets ils mettent phi-1000000-[0-69]_[0-52].jpg de 256*256 
+	 * les tiles de 256*256 dans assets/ au format phi-1000000-[0-69]_[0-52].jpg  
 	 * 69*256 = 17920
 	 * 52*256 = 13312
 	 * 
@@ -83,7 +77,7 @@ public class TileViewDemoSimple extends Activity {
 	 NORTH = Math.toDegrees(Math.atan(Math.sinh(Math.PI * (1 - 2 * (double)row_0/zoom))));
 	 SOUTH = Math.toDegrees(Math.atan(Math.sinh(Math.PI * (1 - 2 * (double)(row_0+n_tiles_y)/zoom))));
 	 
-	 Log.d("vvnx", "onCreate, essai_WEST=" + WEST + " essai_EAST=" + EAST + " essai_NORTH=" + NORTH + " essai_SOUTH=" + SOUTH);
+	 Log.d("vvnx", "onCreate, mes boundaries calculées: WEST=" + WEST + " EAST=" + EAST + " NORTH=" + NORTH + " SOUTH=" + SOUTH);
 
     tileView = findViewById(R.id.tileview);
     new TileView.Builder(tileView)
@@ -139,14 +133,6 @@ public class TileViewDemoSimple extends Activity {
 	} 
 	//Log.d("vvnx", "onResume, coordinates[1]="+coordinates[1]+" coordinates[0]="+coordinates[0]+"et on va mettre le marker a x=" + x + " et y=" + y);
 	markerPlugin.updateMarkerPos(x, y);
-	/*CoordinatePlugin coordinatePlugin = tileView.getPlugin(CoordinatePlugin.class);
-    MarkerPlugin markerPlugin = tileView.getPlugin(MarkerPlugin.class);    
-	int x = coordinatePlugin.longitudeToX(coordinates[1]);
-	int y = coordinatePlugin.latitudeToY(coordinates[0]);
-	Log.d("vvnx", "le marker a x=" + x + " et y=" + y);
-	ImageView marker = new ImageView(this);
-	marker.setImageResource(R.drawable.marker);
-	markerPlugin.addMarker(marker, x, y, -0.5f, -1f, 0, 0);*/
 	
   }
   
@@ -154,7 +140,7 @@ public class TileViewDemoSimple extends Activity {
     public void recup_latlng_fichier(){
 	/**récup des coordonnées qui vont servir à créer marker, dans un fichier
     permissions pour lire sdcard: manifest: android.permission.READ_EXTERNAL_STORAGE
-    ***et***
+    ***et après chaque nvelle install***
     pm grant tileview.demo android.permission.READ_EXTERNAL_STORAGE**/
     
 	File fichier = new File("/sdcard/gps.txt");    
